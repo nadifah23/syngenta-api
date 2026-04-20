ac4ec926bcb06f1c546d8cd74cea8a111775653a
nadifah23 <n23nadifah@gmail.com>
2026-04-20 11:26:36 +0700

    fix railway build

--- obj/Debug/net10.0/Syngenta.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/Syngenta.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8195c2fb77e111b40a4f0aeb14f4d77bbb40426b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
