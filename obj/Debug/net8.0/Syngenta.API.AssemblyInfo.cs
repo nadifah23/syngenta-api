0377128156a8b1d9178642eb4be11b80cc59f841
nadifah23 <n23nadifah@gmail.com>
2026-04-23 08:19:00 +0700

    add upload datasheet

--- obj/Debug/net8.0/Syngenta.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Syngenta.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7a39fdfb1828b99fdc2aa615caa62de4aad203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cc84698b3752d870a716dbd02665d71c3fa774")]
 [assembly: System.Reflection.AssemblyProductAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Syngenta.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
